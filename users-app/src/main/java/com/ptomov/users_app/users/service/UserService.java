b54b4456beddf835e6dfb03389d2b4d0804b1c21
Patrik Tomov <patriktomov@gmail.com>
2025-02-12 11:36:37 +0100

    Created UserService

---
+++ users-app/src/main/java/com/ptomov/users_app/users/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ptomov.users_app.users.service;
+
+import com.ptomov.users_app.users.UserDO;
+
+public interface UserService {
+
+    UserDO getUserById(Long id);
+}
---
+++ users-app/src/main/java/com/ptomov/users_app/users/service/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ptomov.users_app.users.service;
+
+import com.ptomov.users_app.users.UserDO;
+import com.ptomov.users_app.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDO getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
